995a40c3a0e674ed46acb1af72ff0818173a9088
Chris Griffing <chris.griffing@freshconsulting.com>
2018-11-14 16:03:21 -0800

    Update example code a bit

--- example/angularjs-app/app/view1/view1.js
+++ example/angularjs-app/app/view1/view1.js
@@ -11,11 +11,22 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
 
-  $scope.index = 1;
+  $scope.wolfImage = "'sheep.jpg'";
+  $scope.sheepImage = 'sheep.jpg';
 
-  $scope.onRevealed = function(event) {
-    console.log('event', event);
-    //alert(event);
-    $scope.index++;
+  $scope.sheep = [...new Array(8)].map(() => {
+    // Uses random value to decide which element to turn into a wolf
+    return Math.random() > 0.2;
+  });
+
+
+  $scope.saySomething = (event) => {
+    console.log(event.detail, event.target.tagName);
+    if(event.target.tagName === 'NG-WOLF') {
+      $scope.wolfImage = 'wolf.jpg';
+      alert(event.detail);
+    } else {
+      event.target.style['opacity'] = 0.5;
+    }
   }
 }]);
